e9233939a9a5321ed3636507e7592709dca37157
mnebot <miguelnbt@icloud.com>
2021-09-28 20:06:41 -0400

    getOneReceipt added

--- client/src/screens/Receipt/Receipt.jsx
+++ client/src/screens/Receipt/Receipt.jsx
@@ -1,8 +1,26 @@
+import { getOneReceipt } from '../../services/receipt';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './Receipt.css'
+
 const Receipt = () => {
+  const [receipt, setReceipt] = useState({});
+  const { id } = useParams()
+  
+  useEffect(() => {
+    const getReceipt = async () => {
+      const oneReceipt = await getOneReceipt(id);
+      setReceipt(oneReceipt);
+    }
+    getReceipt()
+  },[id]);
+  
   return (
     <div>
-      
+      <h1>{receipt?.title}</h1>
+      <h2>{receipt?.date}</h2>
+      <p>{receipt?.note}</p>
+      <img src={receipt?.images} alt={receipt?.title} />
     </div>
   );
 };
